224f957ea6541f57ecdc6cb6fcf8988a3b972cc9
tahaproo <44582827+tahaproo@users.noreply.github.com>
2018-11-09 13:26:40 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "+";
+var prefix = ".";
 client.on('message', message => {
     if (message.content == "^fast") {
         var x = ["DreamKing",
